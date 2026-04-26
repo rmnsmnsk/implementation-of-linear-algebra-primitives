01dcf08cc0bad297965aea1e756e812e8549bd77
Roman Semensky <roltiserplot@gmail.com>
2026-04-26 14:30:28 +0300

    Changed COO.c and also added CI

--- src/COO.h
+++ src/COO.h
@@ -1,5 +1,5 @@
 #pragma once
-#include<stdbool.h>
+#include <stdbool.h>
 
 typedef struct COO {
   int nnz;           // кол-во ненулевых элементов
@@ -9,13 +9,13 @@ typedef struct COO {
   int *coll_indices;
   float *values; // сами значения
 } COO;
-void free_matrix(COO* matrix);
-COO* create_matrix_copy(COO *original);
+void free_matrix(COO *matrix);
+COO *create_matrix_copy(COO *original);
 COO *sort_matrix(COO *matrix);
 bool is_line(COO *matrix);
 float *make_table_vector(COO *vector);
 COO *multiplication_two_matrix(COO *first, COO *second);
 COO *multiplication_matrix_and_vector(COO *first, COO *second);
 COO *multiplication_vector_and_matrix(COO *first, COO *second);
-void coo_map(COO* mat, float (*func)(float));
-COO* coo_map2(COO* first, COO* second, float (*func)(float, float));
+void coo_map(COO *mat, float (*func)(float));
+COO *coo_map2(COO *first, COO *second, float (*func)(float, float));
